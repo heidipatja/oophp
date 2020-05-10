a8bee70f149f2814ba105f0c30a274b35d276d01
Heidi Patja <hpatja@gmail.com>
2020-05-10 17:55:46 +0200

    Fixed filter and added kmom06 report

--- src/MyTextFilter/MyTextFilter.php
+++ src/MyTextFilter/MyTextFilter.php
@@ -91,7 +91,7 @@ class MyTextFilter
         return preg_replace_callback(
             '#\b(?<![href|src]=[\'"])https?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#',
             function ($matches) {
-                return "<a href=\'{$matches[0]}\'>{$matches[0]}</a>";
+                return "<a href='{$matches[0]}\'>{$matches[0]}</a>";
             },
             $text
         );
